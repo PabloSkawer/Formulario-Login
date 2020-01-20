e855bf039aa82298468cb09495643517211eea39
unknown <deschouwer.pablo@gmail.com>
2020-01-20 09:03:48 +0100

    FFinal

--- codiguito.js
+++ codiguito.js
@@ -1,5 +1,3 @@
-
-var contraseñas=0;
 function mostrarContra(){
     var mostrar = document.getElementById("ContLog");
 
@@ -11,24 +9,6 @@ function mostrarContra(){
     }
 
 }
-/*
-document.getElementById("enviar1").addEventListener("click", comprobarContraseña);
-
-function comprobarContraseña(){
-    event.preventDefault();
-    var contraseña1=document.getElementById("Contraseña").value;
-    var contraseña2=document.getElementById("Repetir").value;
-    var correo=document.getElementById("correo")
-
-    if (contraseña1 == contraseña2){
-        contraseñas=1;
-    }
-    else {
-        contraseñas=2;
-    }
-
-}*/
-
 
 document.getElementById("logear").addEventListener("click",function(){
     document.getElementById("logearse").style.display="block";
@@ -45,40 +25,35 @@ document.getElementById("alta").addEventListener("click",function(){
 });
 document.getElementById("final").addEventListener("click",function(){
     document.getElementById("final").style.display="block";
+    document.getElementById("registrarse").style.display="none";
     document.getElementById("logearse").style.display="none";
     document.getElementById("inicial").style.display="none";
-    document.getElementById("registrarse").style.display="none";
 });
 
-//function error(mensaje){
- //   document.getElementById("mensajeError").style.display="block";
-  //  document.getElementById("mensajeError").innerHTML=mensaje;
-    //document.getElementById("mensajeError").className="error";     
-//}
-
-document.getElementById("enviar2").addEventListener("click", coacito);
-
-
+document.getElementById("enviar1").addEventListener("click",validar)
 
 function validarPass(){
-    var p1 = document.getElementById("Contraseña").value;
+    var p1 = document.getElementById("contraseña").value;
     var expresion=/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}$/;
     
     if(!expresion.test(p1)){
-        document.getElementById("p").innerHTML="Contraseña no valida";
+        document.getElementById("p").innerHTML="Formato de contraseña incorrecto";
         return false;
     }
-        setCookie("Contraseña",p1,1);
-        return true;
-    
+    setCookie("Contraseña",p1,1);
+    return true;
 }
+
+
 function validarPass2(){
-    var p1 = document.getElementById("Contraseña").value;
-    var p2 = document.getElementById("Repetir").value;
+    var p1 = document.getElementById("contraseña").value;
+    var p2 = document.getElementById("repetir").value;
+    
     if (p1 != p2) {   
         document.getElementById("p").innerHTML="Contraseñas no coinciden";
         return false;
     }
+  
     return true;
 }
 
@@ -89,31 +64,48 @@ function validarMail(){
     
     if(expresion.test(correo)||expresion2.test(correo)){
           setCookie("Correo",correo,1);
+    
         return true;
     }
-    
-    document.getElementById("p").innerHTML="Email no valido";
+  
+    document.getElementById("p").innerHTML="Formato de email incorrecto";
     return false;
+    
 }
 
 
-document.getElementById("enviar1").addEventListener("click",validar);
 
 function validar(e){
     if(validarMail() && validarPass() && validarPass2()){
-            document.getElementById("final").style.display="none";
-            document.getElementById("registrarse").style.display="none";
-            document.getElementById("inicial").style.display="none";
-            document.getElementById("logearse").style.display="block";
-            return true;
+        //Esto es para activar un mensaje de SE HA REGISTRADO CORRECTAMENTE
+        document.getElementById("logearse").style.display="block";
+        document.getElementById("p").style.display="none";
+        document.getElementById("registrarse").style.display="none";
+        document.getElementById("d").innerHTML="Se ha registrado correctamente";
+        return true;    
     }
     else{
         e.preventDefault();
-        return false;
         
+        return false;
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function validarMailito(){
     var correo=document.getElementById("email2").value;
     var expresion=/\w+@\w+\.+[a-z]/;
@@ -157,11 +149,14 @@ else{
 document.getElementById("enviar2").addEventListener("click", conectarse);
 
 function conectarse(){
+    document.getElementById("d").style.display="none";
     var c1=document.getElementById("email2").value;
     var c2=document.getElementById("email").value;
-    var p1 = document.getElementById("Contraseña").value;
+    var p1 = document.getElementById("contraseña").value;
     var p2=document.getElementById("ContLog").value;
+
     if (c1==c2&&p1==p2){
+        document.getElementById("p").style.display="none";
         document.getElementById("final").style.display="block";
         document.getElementById("registrarse").style.display="none";
         document.getElementById("inicial").style.display="none";
@@ -169,38 +164,24 @@ function conectarse(){
     }
 
     else if (c1==c2&&p1!=p2) {
+        document.getElementById("p").style.display="block";
         document.getElementById("p").innerHTML="Contraseña erronea";
         return false;
     }
     else if (c1!=c2&&p1==p2) {
+        document.getElementById("p").style.display="block";
         document.getElementById("p").innerHTML="Email erroneo";
         return false;
     }
     else {
+        document.getElementById("p").style.display="block";
         document.getElementById("p").innerHTML="Email y contraseña erroneos";
         return false;
     }
 
 }
 
-function comprobar(e){
-    var a=getCookie("Correo");
-    var b=getCookie("Contraseña");
-    
-    if(document.getElementById("correo").value==document.getElementById("correo2").value && document.getElementById("pass1").value==document.getElementById("pass2").value){
 
-    document.getElementById("conect").style.display="block";
-    document.getElementById("log").style.display="none";
-    document.getElementById("a").innerHTML="Bienvenido "+document.getElementById("correo").value+" has iniciado sesión con éxito...";
-        
-    }else{
-        document.getElementById("c").innerHTML="No se ha podido establecer conexion, revise los datos...";
-        document.getElementById("c").style.className="error";
-        e.preventDefault();
-    }
-   
-    
-}
 
 function desconectar(){
     
